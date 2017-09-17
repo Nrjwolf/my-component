8bbaff859ef2f02acfd3aeefdac646471900dfcb
Alexander <Nrjwolf@MacBook-Pro-Alexander.local>
2017-09-17 17:19:08 +0300

    init

---
+++ Assets/Scripts/Editor/MyComponentEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MyComponent))]
+[CanEditMultipleObjects]
+public class MyComponentEditor : Editor
+{
+
+    MyComponent _target;
+    MyComponent.Type lastType;
+
+    void OnEnable()
+    {
+        _target = (MyComponent)target; // сохраняем целевой объект как MyComponent
+        lastType = _target.type; // сохраняем текущий тип
+        _target.Init();
+    }
+
+    // Аналогично Update
+    public override void OnInspectorGUI()
+    {
+        // Рисумем UI заданный по умолчанию
+        DrawDefaultInspector();
+
+        // Обновляем, при изменении типа
+        if (lastType != _target.type)
+        {
+            // Обновляем вид
+            _target.UpdateView();
+            // Сбрасываем параметры на те, что сохранены в массиве
+			var parametrs = _target.data.Find(x => x.type == _target.type).parametrs;
+            _target.parametrs = new MyComponent.Parametrs(parametrs);
+            // переназначаем последний тип
+            lastType = _target.type;
+        }
+
+        // Изменяем массив при нажатии на кнопку
+        if (GUILayout.Button("Применить"))
+            ApplyChanges();
+
+        GUI.backgroundColor = new Color(0.5f, 0.5f, 0.5f, 1);
+        if (GUILayout.Button("Сбросить"))
+            ResetChanges();
+
+        // Обновление вида, без затрагивания массива
+        _target.UpdateView(_target.parametrs.color, _target.parametrs.count);
+
+    }
+
+    private void ApplyChanges()
+    {
+        var data = _target.data.Find(x => x.type == _target.type);
+		data.parametrs = _target.parametrs;
+        // data.parametrs = new MyComponent.Parametrs(data.parametrs); // здесь происходит клонирование класса, чтобы мы имели возможность менять параметры вне зависимсти от массива данных
+        data.sprite = _target.GetComponent<Image>().sprite;
+        _target.UpdateView();
+    }
+
+    private void ResetChanges()
+    {
+        var data = _target.data.Find(x => x.type == _target.type);
+        _target.parametrs = new MyComponent.Parametrs(data.parametrs);
+        _target.UpdateView();
+    }
+
+}
---
+++ Assets/Scripts/MyComponent.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Image))] // Запрашиваем (создаем) компонент Image
+public class MyComponent : MonoBehaviour
+{
+    // Типы нашего компоненты
+    public enum Type
+    {
+        CAR,
+        CLOUD,
+        COIN
+    }
+
+    // Данные, которые будут изменяться в зависимости от выбранного компонента
+    [Serializable]
+    public class Data
+    {
+        public Type type;
+        public Sprite sprite;
+        public Parametrs parametrs;
+    }
+
+    [Serializable]
+    public class Parametrs
+    {
+        public int count;
+        public Color color = Color.white;
+
+        public Parametrs(Parametrs obj)
+        {
+            this.color = obj.color;
+            this.count = obj.count;
+        }
+    }
+
+    [Serializable]
+    public class View
+    {
+        public Image image;
+        public Text text;
+    }
+
+    public Type type;
+    public Parametrs parametrs;
+    public List<Data> data;
+    [HideInInspector] public View view;
+
+    void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        // Получаем ссылку на визуальные компоненты
+        if (view.image == null)
+            view.image = GetComponent<Image>();
+        if (view.text == null)
+            view.text = GetComponentInChildren<Text>();
+    }
+
+    public void UpdateView()
+    {
+        var _data = data.Find(x => x.type == type);
+        view.image.sprite = _data.sprite;
+        view.image.color = _data.parametrs.color;
+        view.text.text = _data.parametrs.count.ToString();
+    }
+
+    public void UpdateView(Color color, int count)
+    {
+        view.image.color = color;
+        view.text.text = count.ToString();
+    }
+}
